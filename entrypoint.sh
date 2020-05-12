5ab430d8e913901a842f058436e1e5bdf3a8d897
Chakradhar Rao <chakradhar.jonagam@biqmind.com>
2020-05-13 02:10:29 +0800

    debug messages

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,9 @@
 
 set -e
 
+echo "running entrypoint"
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
-
+echo "about to run command"
 sh -c " $*"
